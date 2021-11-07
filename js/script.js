187775fe13ecb4a8b4ec1dc8ae2422984724df6e
Nikita <nikitakulberden@gmail.com>
2021-11-07 19:45:33 +0200

    normall scroll to anchor

--- js/script.js
+++ js/script.js
@@ -29,6 +29,17 @@ $(function() {
     });
 });
 
+$(function() {
+    $(document).ready(function(){
+        $(".nav").on("click","a", function (event) {
+            event.preventDefault();
+            var id  = $(this).attr('href'),
+                top = $(id).offset().top;
+            $('body,html').animate({scrollTop: top}, 1200);
+        });
+    });
+})
+
 // $(function() {
 //         jQuery(window).scroll(function(){
 
